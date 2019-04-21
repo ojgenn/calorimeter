8c28f4d08a28d5ceb369460127abb4be6f1aabba
ojgenn <ojgenn@gmail.comgit>
2019-04-21 21:15:24 +0300

    add sign-in page

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -7,19 +7,21 @@ import { IonicModule } from '@ionic/angular';
 import { StoreModule } from '@ngrx/store';
 import { TranslateModule } from '@ngx-translate/core';
 
-import {ProfileComponent} from './profile.component';
+import { ProfileComponent } from './profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { profileReducer } from './reducer/reducers/profile.reducer';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
-  declarations: [ProfileComponent],
-  imports: [
-    IonicModule,
-    CommonModule,
-    TranslateModule,
-    ProfileRoutingModule,
-    StoreModule.forFeature('profile', profileReducer),
-    RouterModule.forChild([{ path: '', component: ProfileComponent }])
-  ]
+    declarations: [ProfileComponent, SignInComponent],
+    imports: [
+        IonicModule,
+        CommonModule,
+        TranslateModule,
+        ProfileRoutingModule,
+        StoreModule.forFeature('profile', profileReducer),
+        RouterModule.forChild([{ path: '', component: ProfileComponent }]),
+    ],
 })
-export class ProfileModule { }
+export class ProfileModule {
+}
