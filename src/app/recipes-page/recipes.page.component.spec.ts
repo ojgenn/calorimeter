864aaf76b8c63ce8e167fc835a93d8b161c85633
ojgenn <ojgenn@gmail.comgit>
2019-04-30 11:40:06 +0300

    rename tab2 to recipes-page

--- src/app/recipes-page/recipes.page.component.spec.ts
+++ src/app/recipes-page/recipes.page.component.spec.ts
@@ -1,21 +1,21 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Tab2Page } from './tab2.page';
+import { RecipesPageComponent } from './recipes.page.component';
 
-describe('Tab2Page', () => {
-  let component: Tab2Page;
-  let fixture: ComponentFixture<Tab2Page>;
+describe('RecipesPageComponent', () => {
+  let component: RecipesPageComponent;
+  let fixture: ComponentFixture<RecipesPageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab2Page],
+      declarations: [RecipesPageComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(Tab2Page);
+    fixture = TestBed.createComponent(RecipesPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/recipes-page/recipes.page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-page',
+  templateUrl: 'recipes.page.component.html',
+  styleUrls: ['recipes.page.component.scss']
+})
+export class RecipesPageComponent {}
