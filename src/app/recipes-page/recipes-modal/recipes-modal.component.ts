3773c7655899e7b2038e3840256867f2b7949a76
ojgenn <ojgenn@gmail.comgit>
2019-04-30 21:30:30 +0300

    prepare firestore

--- src/app/recipes-page/recipes-modal/recipes-modal.component.ts
+++ src/app/recipes-page/recipes-modal/recipes-modal.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+
+import { RecipesSegments } from '../commons/enums/recipes-segments.enum';
+import { User } from 'firebase';
 
 @Component({
-  selector: 'app-recipes-modal',
-  templateUrl: './recipes-modal.component.html',
-  styleUrls: ['./recipes-modal.component.scss'],
+    selector: 'app-recipes-modal',
+    templateUrl: './recipes-modal.component.html',
+    styleUrls: ['./recipes-modal.component.scss'],
 })
 export class RecipesModalComponent implements OnInit {
 
-  constructor(private _modalCtrl: ModalController) { }
+    @Input() activeSegment: RecipesSegments;
+    @Input() set uid(id: User['uid']) {
+        this._items = this._afs.collection(`test`);
+    }
 
-  ngOnInit() {}
+    recipesSegments = RecipesSegments;
 
-  close() {
-    this._modalCtrl.dismiss().catch();
-  }
+    private _items: AngularFirestoreCollection;
+
+    constructor(private _modalCtrl: ModalController,
+                private _afs: AngularFirestore) { }
+
+    ngOnInit() {
+    }
+
+    close() {
+        this._items.doc('last').collection('fest').add({check: 1}).catch();
+        this._modalCtrl.dismiss().catch();
+    }
 }
--- src/app/recipes-page/recipes.page.component.ts
+++ src/app/recipes-page/recipes.page.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { RecipesModalComponent } from './recipes-modal/recipes-modal.component';
-
-export enum RecipesSegments {
-    Products,
-    Sport,
-}
+import { RecipesSegments } from './commons/enums/recipes-segments.enum';
+import { ObservableHandler } from '../shared/models/observable-handler';
+import { user } from '../reducer';
+import { Store } from '@ngrx/store';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { User } from 'firebase';
 
 @Component({
     selector: 'app-recipes-page',
     templateUrl: 'recipes.page.component.html',
     styleUrls: ['recipes.page.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class RecipesPageComponent {
@@ -21,17 +23,42 @@ export class RecipesPageComponent {
     segments = RecipesSegments;
     activeSegment: RecipesSegments = RecipesSegments.Products;
 
-    constructor(public _modalController: ModalController) {}
+    // удалить, когда начнем качать реальные данные с сервера
+    items = Array.from(Array(1000).keys());
+
+    private _user = new ObservableHandler(
+        this._store.select(user),
+        this._initCollection.bind(this),
+        this._cdr,
+    );
+
+    private _items: AngularFirestoreCollection<any>;
+
+    constructor(public _modalController: ModalController,
+                private _store: Store<any>,
+                private _cdr: ChangeDetectorRef,
+                private _afs: AngularFirestore) {}
 
     segmentChanged(ev: any) {
-        this.activeSegment = ev;
+        this.activeSegment = ev.detail.value as RecipesSegments;
     }
 
     async presentModal() {
+        console.log(this._user.latestValue);
         const modal: HTMLIonModalElement = await this._modalController.create({
             component: RecipesModalComponent,
-            componentProps: { activeSegment: this.activeSegment }
+            componentProps: {
+                activeSegment: this.activeSegment,
+                uid: this._user.latestValue.uid,
+            },
         });
         return await modal.present();
+    }
+
+    private _initCollection(userFromStore: User): void {
+        console.log(userFromStore);
+        if (!!userFromStore) {
+            this._items = this._afs.collection('test');
+        }
     }
 }
