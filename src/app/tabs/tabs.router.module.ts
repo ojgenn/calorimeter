b8e73d1e0931026d1c4300d7abb9e7fcd89659d8
ojgenn <ojgenn@gmail.comgit>
2019-05-02 09:03:23 +0300

    rename tabs3 to calorimeter component

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -28,12 +28,12 @@ const routes: Routes = [
                 ],
             },
             {
-                path: 'tab3',
+                path: 'calorimeter',
                 canActivate: [AuthGuard],
                 children: [
                     {
                         path: '',
-                        loadChildren: '../tab3/tab3.module#Tab3PageModule',
+                        loadChildren: '../calorimeter/calorimeter.module#CalorimeterModule',
                     },
                 ],
             },
