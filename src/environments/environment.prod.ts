6bf950d586e77079677e345f91a36de0afb5eb0c
ojgenn <ojgenn@gmail.com>
2019-05-06 19:19:54 +0300

    set firebase settings for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,12 @@ export const environment = {
     production: true,
     language: Languages.Ru,
     useNgRxDevTool: false,
+    firebase: {
+        apiKey: 'AIzaSyDhJygQbGEHRAL0Tj61HaPy3qqL-fKVhww',
+        authDomain: 'calorimeter-24409.firebaseapp.com',
+        databaseURL: 'https://calorimeter-24409.firebaseio.com',
+        projectId: 'calorimeter-24409',
+        storageBucket: 'calorimeter-24409.appspot.com',
+        messagingSenderId: '483282447747',
+    },
 };
