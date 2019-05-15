e572418f430b1c2d5d6ab024df27948dc3f71a49
ojgenn <ojgenn@gmail.comgit>
2019-05-15 23:01:50 +0300

    change authorization and registration

--- src/app/profile/services/auth.service.ts
+++ src/app/profile/services/auth.service.ts
@@ -7,6 +7,7 @@ import { auth, User } from 'firebase';
 import * as ProfileActions from '../reducer/actions/index';
 
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root',
@@ -15,7 +16,7 @@ export class AuthService {
 
     static readonly instance: AuthService;
 
-    constructor(public afAuth: AngularFireAuth, private _store: Store<any>) {
+    constructor(public afAuth: AngularFireAuth, private _store: Store<any>, private _http: HttpClient) {
         // @ts-ignore
         // noinspection TypeScriptUnresolvedVariable
         AuthService.instance = this;
@@ -28,11 +29,15 @@ export class AuthService {
             .catch(error => this._store.dispatch(new ProfileActions.SignOutFailure()));
     }
 
-    signIn(): void {
-        this._store.dispatch(new ProfileActions.SignIn());
-        this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
-            .then(res => this._store.dispatch(new ProfileActions.SignInSuccess(res.user)))
-            .catch(error => this._store.dispatch(new ProfileActions.SignInFailure()));
+    // signIn(): void {
+    //     this._store.dispatch(new ProfileActions.SignIn());
+    //     this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
+    //         .then(res => this._store.dispatch(new ProfileActions.SignInSuccess(res.user)))
+    //         .catch(error => this._store.dispatch(new ProfileActions.SignInFailure()));
+    // }
+
+    signUp(body): Observable<any> {
+        return this._http.post('http://localhost:8080/auth/sign-up', body);
     }
 
     getUser(): Observable<User> {
--- src/app/profile/sign-in/sign-in.component.ts
+++ src/app/profile/sign-in/sign-in.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 import { Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
 
+import { Store } from '@ngrx/store';
 import { AuthService } from '../services/auth.service';
 import { authPending } from '../../reducer';
 import { PendingStatuses } from '../../shared/enums/pending-statuses.enum';
+
+enum authPurposeEnum {
+  Auth = 'auth',
+  Register = 'register',
+}
+
+function confirmPasswordValidation(
+    control: FormGroup
+): { [key: string]: any } | null {
+  if (!control.get('confirmPassword')) {
+    return null;
+  }
+  const valid = control.get('password').value === control.get('confirmPassword').value;
+  return valid
+      ? null
+      : { invalidConfirmPassword: { valid: false, value: control.value } };
+}
 
 @Component({
   selector: 'app-sign-in',
@@ -15,10 +39,52 @@ import { PendingStatuses } from '../../shared/enums/pending-statuses.enum';
 export class SignInComponent {
   authPending$: Observable<PendingStatuses> = this._store.select(authPending);
   pendingStatuses = PendingStatuses;
+  authForm: FormGroup = this._initAuthFormGroup();
+  authPurpose = authPurposeEnum;
+  signInChecker: authPurposeEnum = this.authPurpose.Auth;
 
-  constructor(private _store: Store<any>, private _authService: AuthService) {}
+  constructor(private _store: Store<any>,
+              private _authService: AuthService,
+              private _formBuilder: FormBuilder) {}
 
   signIn() {
-    this._authService.signIn();
+    switch (this.signInChecker) {
+      case authPurposeEnum.Auth:
+        console.log('sign-in')
+        break;
+      case authPurposeEnum.Register:
+        this._authService.signUp({
+          email: this.authForm.controls.email.value,
+          password: this.authForm.controls.password.value,
+        }).subscribe(res => console.log(res));
+        break;
+    }
+  }
+
+  segmentChanged(ev: any): void {
+    switch (ev.detail.value) {
+      case this.authPurpose.Auth:
+        this.signInChecker = this.authPurpose.Auth;
+        if (this.authForm.controls.confirmPassword) {
+          this.authForm.removeControl('confirmPassword');
+        }
+        break;
+      case this.authPurpose.Register:
+        this.signInChecker = this.authPurpose.Register;
+        if (!this.authForm.controls.confirmPassword) {
+          this.authForm.addControl('confirmPassword',
+              new FormControl('',
+                  [Validators.required, Validators.minLength(5)]));
+        }
+        break;
+    }
+  }
+
+  private _initAuthFormGroup(): FormGroup {
+    return this._formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(5)])
+    }, {validators: confirmPasswordValidation});
   }
 }
+
